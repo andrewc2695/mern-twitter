178cb61c2d16c4923cc3ecbf49e76ea0f8e68897
Andrew Curd <andrewc2695@gmail.com>
2021-02-20 13:14:51 -0800

    created new routes for user

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose');
 const express = require("express");
+const bodyParser = require('body-parser');
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 const app = express();
-app.get("/", (req, res) => res.send("Sydney"));
+const passport = require('passport');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize())
+require('./config/passport')(passport);
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
 const db = require('./config/keys').mongoURI;
 const port = process.env.PORT || 5000;
+
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
